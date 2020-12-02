f7e756c9ec303e10b1cd25f25586a6aba6264ae1
Ilya <ilya@day100.me>
2020-12-02 17:45:45 -0500

    updates

--- lib/bot.ts
+++ lib/bot.ts
@@ -43,9 +43,8 @@ class SlackBot {
       count: 100,
       token: this.userToken
     });
-    console.log("46", filename)
-    console.log("47", files)
     const indexOfFileName = files.length && files.findIndex(i => i.name === filename)
+    console.log("indexOfFileName", indexOfFileName)
     return indexOfFileName ? files[indexOfFileName] : null;
   }
 
